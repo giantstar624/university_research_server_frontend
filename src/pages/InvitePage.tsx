6f978ee3fbc740f071af8363509ae6a7e12cd794
giantstar624 <brightcn624@gmail.com>
2024-08-11 17:02:13 -0700

    update

--- src/pages/InvitePage.tsx
+++ src/pages/InvitePage.tsx
@@ -1,15 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios"
-import { useState } from "react";
 const backendUrl = import.meta.env.VITE_BACKEND_URL;
 const InvitePage = () => {
-    const [email, setEmail] = useState("");
-    const invite = () => {
-        axios.get(`${backendUrl}/invite`, {params: { email }})
+    const navigate = useNavigate();
+    const onClick = async () => {
+        const result = (await axios.get(`${backendUrl}/launch`)).data;
+        if (result.launched == true) {
+            navigate(result.url);
+        } else alert('All instaces are running. Wait a min');
     }
     return <div className="w-full h-full flex items-center justify-center relative">
         <div className="flex flex-col items-center gap-5">
-            <a href={backendUrl + "/launch"} className="text-blue-500 text-3xl underline">Participate to the study</a>
+            <a className="text-blue-500 text-3xl underline" onClick={onClick}>Participate to the study</a>
         </div>
         <div className="right-10 bottom-10 absolute">
             <Link to="/ids">Go to Log page</Link>
